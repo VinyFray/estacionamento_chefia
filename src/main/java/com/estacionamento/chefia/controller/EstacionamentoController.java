b919805c165eac47af174681a304b720df3835ea
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-09-11 14:34:48 -0300

    Create cadastrar carro method

--- src/main/java/com/estacionamento/chefia/controller/EstacionamentoController.java
+++ src/main/java/com/estacionamento/chefia/controller/EstacionamentoController.java
@@ -1,14 +1,15 @@
 package com.estacionamento.chefia.controller;
 
 import com.estacionamento.chefia.controller.dtos.CarroDTO;
+import com.estacionamento.chefia.controller.dtos.Placa;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/estacionamento")
@@ -19,5 +20,22 @@ public class EstacionamentoController {
     @GetMapping
     public ResponseEntity<?> exibirEstacionamento(){
         return ResponseEntity.ok(ESTACIONAMENTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> cadastrarCarro(@RequestBody Placa placa){
+       Optional<CarroDTO> carroDTOOptional = ESTACIONAMENTO.stream()
+               .filter(carroDTO -> carroDTO.getPlaca().equalsIgnoreCase(placa.getPlaca())).findFirst();
+
+       if(carroDTOOptional.isPresent()){
+           return ResponseEntity.status(400).body(Map.of("mensagem", "Carro já cadastrado"));
+       }
+
+       CarroDTO carroDTO = new CarroDTO();
+       carroDTO.setPlaca(placa.getPlaca());
+       carroDTO.setHoraEntrada(LocalDateTime.now());
+
+       ESTACIONAMENTO.add(carroDTO);
+       return ResponseEntity.status(201).body(carroDTO);
     }
 }
---
+++ src/main/java/com/estacionamento/chefia/controller/dtos/Placa.java
@@ -0,0 +1,16 @@
+package com.estacionamento.chefia.controller.dtos;
+
+public class Placa {
+    private String placa;
+
+    public Placa() {
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
