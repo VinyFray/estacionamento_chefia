f92a8c6acc8c59ffe5a6a732657f3f4237ddef3e
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-09-11 13:49:38 -0300

    First commit

---
+++ src/main/java/com/estacionamento/chefia/EstacionamentoChefiaApplication.java
@@ -0,0 +1,13 @@
+package com.estacionamento.chefia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoChefiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoChefiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estacionamento/chefia/EstacionamentoChefiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estacionamento.chefia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoChefiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
